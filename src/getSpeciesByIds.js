70a2472efa3d9370644244812c14a1add0752b7d
Thais Rosa <tsrosa18@gmail.com>
2022-07-28 19:07:18 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,8 @@
 const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) { //uso o rest para a função poder receber vários parâmetros
-  return species.filter((element) => //esse vai filtrar os elementos que tem o id correspondentes
+function getSpeciesByIds(...ids) { // uso o rest para a função poder receber vários parâmetros
+  return species.filter((element) => // esse vai filtrar os elementos que tem o id correspondentes
     ids.some((id) => (element.id === id))); // quero testar se é true que o id que eu colocar como parâmetro vai corresponder com o id do elemento
 }
 
