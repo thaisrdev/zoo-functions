5bf647d8a4608f04c575c96069500829fe8589a9
Thais Rosa <tsrosa18@gmail.com>
2022-07-28 19:06:46 -0300

    requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -9,4 +9,3 @@ function getSpeciesByIds(...ids) { //uso o rest para a função poder receber v�
 console.log(getSpeciesByIds('0938aa23-f153-4937-9f88-4858b24d6bce'));
 
 module.exports = getSpeciesByIds;
-
