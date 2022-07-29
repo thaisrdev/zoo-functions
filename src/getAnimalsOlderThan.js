00dd749088c915659c279df8498a1c4a6bbe8ced
Thais Rosa <tsrosa18@gmail.com>
2022-07-29 04:17:04 -0300

    requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,7 +2,12 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  // EVERY >=age
+  return species.find((element) => element.name === animal).residents.every(
+    (resident) => resident.age >= age,
+  );
 }
+
+console.log(getAnimalsOlderThan('lions', 7));
+console.log(getAnimalsOlderThan('tigers', 19));
 
 module.exports = getAnimalsOlderThan;
