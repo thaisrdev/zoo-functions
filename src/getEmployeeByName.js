41748fef5bb13ac80a8cec04dd3b1df13660c8bc
Thais Rosa <tsrosa18@gmail.com>
2022-07-29 05:22:54 -0300

    corrigindo erros no lint req3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,5 +1,4 @@
-// eslint-disable-next-line no-unused-vars
-const { species, employees } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
