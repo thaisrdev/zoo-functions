42df4776ffa7c9f3dada185c8f873dc569215d4a
Thais Rosa <tsrosa18@gmail.com>
2022-07-29 05:18:08 -0300

    requisito 3 finalizado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,8 +3,9 @@ const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // eslint-disable-next-line max-len
-  return employees.find((element) => (employeeName === element.firstName) || (employeeName === element.lastName));
+  if (employeeName === undefined) { return {}; }
+  return employees.find((element) => (
+    employeeName === element.firstName) || (employeeName === element.lastName));
 }
 console.log(getEmployeeByName('Nigel'));
 console.log(getEmployeeByName('Elser'));
