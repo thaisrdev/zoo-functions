ba51c950e3af8421af688fe54f54537c9facedb9
Thais Rosa <tsrosa18@gmail.com>
2022-07-29 21:12:59 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,20 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return employees.some((element) => (element.managers).some((manager) => manager === id));
 }
 
+console.log(isManager('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
+
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if ((isManager(managerId) === false)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } else if (isManager(managerId) === true) {
+    const employee = employees.filter((element) => (element.managers).includes(managerId));
+    return employee.map((element) => `${element.firstName} ${element.lastName}`);
+  }
 }
+console.log(getRelatedEmployees('fdb2543b-5662-46a7-badc-93d960fdc0a8'));
 
 module.exports = { isManager, getRelatedEmployees };
