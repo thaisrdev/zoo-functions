f6afbe0609d471487746ca3e7df01c8cc673da0b
Thais Rosa <tsrosa18@gmail.com>
2022-07-31 15:03:30 -0300

    requisitos 6 e 7 handlerElephants

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+// return species.find((element) => element.residents.length).find((element) => element.name === animal)
+  return species;
 }
+console.log(countAnimals('lions'));
 
 module.exports = countAnimals;
+
+// Implemente a função countAnimals que deverá contabilizar a quantidade de espécies de animais residentes no zoológico.
+
+// A função countAnimals é responsável por contar a quantidade de animais que residem no zoológico.
+
+// Retorne a quantidade de animais residentes por espécie caso não seja passado nenhum parâmetro.
+//  O retorno deverá ser um objeto cujo o nome de cada espécie é a chave e o total de animais (residentes) dessa espécie é o valor. Por exemplo:
+
+//   {
+//     lions: 4,
+//     // [...]
+//   }
+// Retorne a quantidade de animais residentes no zoológico da espécie passada por parâmetro. Por exemplo:
+
+// ao receber o argumento { specie: 'penguins' }, retorna apenas a quantidade (número) de pinguins que residem no zoológico;
+
+// ao passar o argumento { specie: 'giraffes', sex: 'female' }, retorna apenas a quantidade (número) de girafas fêmeas que residem no zoológico.
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,25 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('checa se o argumento count retorna a quantidade de elefantes', () => {
+    expect(handlerElephants('count')).toEqual(4);
+  });
+  it('checa se argumento names retorna um array com a relação dos nomes de todos os elefantes', () => {
+    expect(handlerElephants('names')).toEqual(expect.arrayContaining(['Ilana', 'Orval', 'Bea', 'Jefferson']));
+  });
+  it('checa se o argumento averageAge retorna a média de idade dos elefantes', () => {
+    expect(handlerElephants('averageAge')).toBeCloseTo(10.5);
+  });
+  it('checa se o argumento location retorna a localização dos elefantes dentro do Zoológico', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+  it('checa se argumento popularity retorna a popularidade dos elefantes', () => {
+    expect(handlerElephants('popularity')).toEqual(5);
+  });
+  it('checa se o argumento availability retorna um array com a relação de dias em que é possível visitar os elefantes', () => {
+    expect(handlerElephants('availability')).toEqual(expect.arrayContaining(['Friday', 'Saturday', 'Sunday', 'Tuesday']));
+  });
+  it('Não passando argumentos a função deve retornar undefined', () => {
+    expect(handlerElephants('')).toBeNull();
+  });
+});
