dfbf5a0d975759b79456fccd25d72d69936be575
Thais Rosa <tsrosa18@gmail.com>
2022-08-02 18:10:00 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,41 @@
 const data = require('../data/zoo_data');
 
+const entrants = [
+  { name: 'Lara Carvalho', age: 5 },
+  { name: 'Frederico Moreira', age: 5 },
+  { name: 'Pedro Henrique Carvalho', age: 5 },
+  { name: 'Maria Costa', age: 18 },
+  { name: 'Núbia Souza', age: 18 },
+  { name: 'Carlos Nogueira', age: 50 },
+];
 function countEntrants(entrants) {
-  // seu código aqui ffgit
+  const contagem = entrants.reduce((acc, { age }) => {
+    if (age < 18) {
+      acc.child += 1;
+    }
+    if (age >= 18 && age < 50) {
+      acc.adult += 1;
+    }
+    if (age >= 50) {
+      acc.senior += 1;
+    }
+    return acc;
+  }, { child: 0, adult: 0, senior: 0 });
+
+  return contagem;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.entries(entrants).length === 0) {
+    return 0;
+  }
+  const valores = countEntrants(entrants);
+  const priceChild = data.prices.child;
+  const priceAdult = data.prices.adult;
+  const priceSenior = data.prices.senior;
+  return ((valores.child * priceChild) + (valores.adult * priceAdult) + (valores.senior * priceSenior));
 }
+
+countEntrants(entrants);
 
 module.exports = { calculateEntry, countEntrants };
