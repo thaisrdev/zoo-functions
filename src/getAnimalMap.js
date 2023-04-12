ae8c905348cbace66b3a84c4db613fe13b9b12f5
Thais Rosa <tsrosa18@gmail.com>
2023-04-11 21:22:53 -0300

    publicação do projeto

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,24 @@
 const data = require('../data/zoo_data');
 
 function getAnimalMap(options) {
-  // seu código aqui
+if (options.includes(includeNames)) {
+
+} else if (options.includes(sorted)) {
+
+}else if (options.includes(male)) {
+
+} else if (options.includes(female)) {
+
+} else {
+
+}
+}
+
+{
+  NE: [ /* dados aqui */],
+  NW: [/* dados aqui */],
+  SE: [/* dados aqui */],
+  SW: [/* dados aqui */],
 }
 
 module.exports = getAnimalMap;
